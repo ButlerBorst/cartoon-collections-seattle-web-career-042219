a180af7e7e23fceed51df360580831e12f5747cb
Alex Borst <butler.borst@gmail.com>
2019-04-15 23:10:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  if array.include? (cheese_types)
-    puts 
   cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
 end
